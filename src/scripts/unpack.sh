8cd5e157b32f9fd9cde1e50c68e707416634077d
mspancho <157725058+mspancho@users.noreply.github.com>
2025-05-02 20:51:01 -0400

    init dirs structure

---
+++ src/scripts/unpack.sh
@@ -0,0 +1,14 @@
+# Unpack DG-Miner data tsv file (tab-separated values)
+gzip -d DG-Miner_miner-disease-gene.tsv.gz
+
+# Create directories
+mkdir -p src
+mkdir -p src/data/DG-Miner-tsv
+mkdir -p src/models
+mkdir -p src/figures
+mkdir -p src/scripts
+
+# Move files to appropriate directories
+mv unpack.sh src/scripts/unpack.sh
+mv DG-Miner_miner-disease-gene.tsv src/data/DG-Miner-tsv/mine-disease-gene.tsv
+mv graph_construction.py src/scripts/graph_construction.py
